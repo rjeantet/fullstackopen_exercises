54f3ec3548d1cefb40260e6dfd328dd015a4f840
rjeantet <rose.jeantet@gmail.com>
2023-10-19 19:17:00 +0200

    2.18* Data for countries, step1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import countryService from './services/countries';
+import Country from './components/Country';
 
 const App = () => {
-  return <div>App</div>;
+  const [search, setSearch] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    console.log('fetching countries...');
+    countryService.getAll().then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+    const filteredCountries = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredCountries(filteredCountries);
+  };
+
+  return (
+    <>
+      <label>Find countries: </label>
+      <input value={search} onChange={handleSearch} />
+
+      <ul>
+        {filteredCountries.length > 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : filteredCountries.length === 1 ? (
+          <Country filteredCountries={filteredCountries} />
+        ) : (
+          filteredCountries.map((country, index) => (
+            <li key={country.cioc || index}>{country.name.common}</li>
+          ))
+        )}
+      </ul>
+    </>
+  );
 };
 
 export default App;
---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,44 @@
+import countryService from '../services/countries';
+import { useState, useEffect } from 'react';
+
+const Country = ({ filteredCountries }) => {
+  const [countryDetails, setCountryDetails] = useState({
+    name: { common: '' },
+    capital: '',
+    area: 0,
+    languages: [],
+    flags: { png: '' },
+  });
+
+  useEffect(() => {
+    const name = filteredCountries[0].name.common;
+    console.log(name);
+    countryService.getCountry(name).then((response) => {
+      setCountryDetails(response.data);
+      console.log('country details:', response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2>{countryDetails.name.common}</h2>
+      <div>capital {countryDetails.capital}</div>
+      <div>area {countryDetails.area}</div>
+      <h3>languages</h3>
+      <ul>
+        {Object.values(countryDetails.languages).map((language, index) => (
+          <li key={index}>{language}</li>
+        ))}
+      </ul>
+      <p>
+        <img
+          src={countryDetails.flags.png}
+          alt={`${countryDetails.name.common} flag`}
+          width='200'
+        />
+      </p>
+    </>
+  );
+};
+
+export default Country;
