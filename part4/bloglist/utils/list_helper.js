73e3f9a471f805bc7f34378c5b9a8899191a19ff
rjeantet <rose.jeantet@gmail.com>
2023-10-26 13:38:55 +0200

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
 };
 
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((max, blog) => (max.likes > blog.likes ? max : blog));
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
