cb73bfd0fcf7426a7705798f460f5e9558f3c220
rjeantet <rose.jeantet@gmail.com>
2023-10-31 15:05:21 +0100

    4.9: Blog list tests, step2

--- part4/bloglist/tests/blogs._api.test.js
+++ part4/bloglist/tests/blogs._api.test.js
@@ -17,6 +17,11 @@ test('there are two blogs', async () => {
   expect(response.body).toHaveLength(2);
 });
 
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body[0].id).toBeDefined();
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
