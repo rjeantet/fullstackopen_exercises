460cf2958e1047316fb735851822fee2a8d12f80
rjeantet <rose.jeantet@gmail.com>
2023-11-01 17:36:41 +0100

    4.15: bloglist expansion, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,8 @@ const app = express();
 const cors = require('cors');
 
 const blogsRouter = require('./controllers/blogs');
+const loginRouter = require('./controllers/login');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -27,6 +29,8 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
