9aa62aee5ec029861b49cd7f0c53d7977634dcb2
rjeantet <rose.jeantet@gmail.com>
2023-11-01 19:52:51 +0100

    4.20*: bloglist expansion, step8

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -28,8 +28,18 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    const token = authorization.replace('Bearer ', '');
+    request.token = token;
+  }
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
