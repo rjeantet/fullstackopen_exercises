8f738be070952246fdaac748bdc717e656ec758a
rjeantet <rose.jeantet@gmail.com>
2023-11-02 15:21:10 +0100

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false);
+
+  const handleShowDetails = () => {
+    setDetails(!details);
+  };
+
+  const blogStyle = {
+    padding: 10,
+    margin: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 10,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={handleShowDetails}>{details ? 'hide' : 'show'}</button>
+      <div>
+        {details && (
+          <>
+            <div>
+              <a href={blog.url}>{blog.url}</a>
+            </div>
+            <div>
+              likes {blog.likes}
+              <button>like</button>
+            </div>
+            <div>{blog.author}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
