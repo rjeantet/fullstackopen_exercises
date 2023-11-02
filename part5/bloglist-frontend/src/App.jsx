1fff3107d58eb8b63f7b8c2223b34df4e7ae9e2d
rjeantet <rose.jeantet@gmail.com>
2023-11-02 11:10:57 +0100

    5.2: bloglist frontend, step2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -19,7 +19,9 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
       console.log(user);
+      blogService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
@@ -35,10 +37,29 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
   return user ? (
     <>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in{' '}
+        <button
+          onClick={() => {
+            window.localStorage.removeItem('loggedBlogappUser');
+            setUser(null);
+          }}
+        >
+          logout
+        </button>
+      </p>
 
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
