9a76ba77ecea149f72dc0d6c29291e76f5a28cf7
rjeantet <rose.jeantet@gmail.com>
2023-11-06 08:09:49 +0100

    5.17 to 5.19

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,23 +1,55 @@
-describe('template spec', function () {
+describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`);
     const user = {
       username: 'rose',
       name: 'rose jeantet',
       password: 'salainen',
     };
-    cy.request('POST', 'http://localhost:3003/api/users/', user);
-    cy.visit('http://localhost:5173');
-    cy.get('#username').type('rose');
-    cy.get('#password').type('salainen');
-    cy.get('#login-button').click();
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user);
+    cy.visit('');
   });
-  it('a new blog can be created', function () {
-    cy.contains('new blog').click();
-    cy.get('#title').type('a blog created by cypress');
-    cy.get('#author').type('cypress');
-    cy.get('#url').type('http://cypress.io');
-    cy.contains('create').click();
-    cy.contains('a blog created by cypress');
+
+  it('Login form is shown', function () {
+    cy.contains('Log in to the application');
+    cy.contains('username');
+    cy.contains('password');
+    cy.contains('login');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('rose');
+      cy.get('#password').type('salainen');
+      cy.get('#login-button').click();
+      cy.contains('rose jeantet logged in').should('exist');
+    });
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('rose');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(128, 0, 0)')
+        .and('have.css', 'border-style', 'solid');
+      cy.contains('Rose Jeantet logged in').should('not.exist');
+    });
+  });
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'rose', password: 'salainen' });
+    });
+
+    it('a new blog can be created', function () {
+      cy.createBlog({
+        title: 'a blog created by cypress',
+        author: 'cypress',
+        url: 'http://cypress.io',
+      });
+      cy.contains('a blog created by cypress');
+      cy.contains('cypress');
+      cy.contains('show').click();
+    });
   });
 });
