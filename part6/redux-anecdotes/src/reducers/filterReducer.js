8ea0706662413df227757847f691ac3355c1d27e
rjeantet <rose.jeantet@gmail.com>
2023-11-09 18:42:03 +0100

    6.11

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -2,24 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = '';
 
-// const filterReducer = (state = initialState, action) => {
-//   console.log('state now: ', state);
-//   console.log('action', action);
-//   switch (action.type) {
-//     case 'SET_FILTER':
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// };
-
-// export const filterChange = (content) => {
-//   return {
-//     type: 'SET_FILTER',
-//     payload: content,
-//   };
-// };
-
 const filterSlice = createSlice({
   name: 'filter',
   initialState,
