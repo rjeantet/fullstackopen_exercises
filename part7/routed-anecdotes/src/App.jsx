be8f61d9848b6bc6922d95d5aca5cf3f95bd1bd7
rjeantet <rose.jeantet@gmail.com>
2023-11-10 21:15:06 +0100

    7.4: anecdotes and hooks step1

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react';
-import {
-  useMatch,
-  Routes,
-  Route,
-  Link,
-  useParams,
-  useNavigate,
-} from 'react-router-dom';
+import { useMatch, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { useField } from './hooks';
 
 const Menu = ({ addNew, anecdotes, notification }) => {
   const match = useMatch('/anecdotes/:id');
@@ -44,8 +38,6 @@ const Menu = ({ addNew, anecdotes, notification }) => {
 };
 
 const Anecdote = ({ anecdote }) => {
-  //const id = useParams().id;
-
   return (
     <>
       <h2>{anecdote.content}</h2>
@@ -107,16 +99,19 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const navigate = useNavigate();
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
+  // const [content, setContent] = useState('');
+  // const [author, setAuthor] = useState('');
+  // const [info, setInfo] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate('/');
@@ -130,24 +125,27 @@ const CreateNew = (props) => {
           content{' '}
           <input
             name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
           />
         </div>
         <div>
           author{' '}
           <input
             name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
           />
         </div>
         <div>
           url for more info{' '}
           <input
             name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            type={info.type}
+            value={info.value}
+            onChange={info.onChange}
           />
         </div>
         <button>create</button>
