e067a8b67c78cd3cb43fe01d1ae2d06fe676e04c
rjeantet <rose.jeantet@gmail.com>
2023-11-22 10:27:15 +0100

    8.2: All books

--- part8/index.js
+++ part8/index.js
@@ -90,9 +90,18 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]!
   }
 `;
 
@@ -100,6 +109,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
