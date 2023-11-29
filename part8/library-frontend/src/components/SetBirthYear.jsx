f15fb5eae9d61c5abf10100f43f0425ec6509d01
rjeantet <rose.jeantet@gmail.com>
2023-11-29 12:24:28 +0100

    8.12: Authors birth year advanced

--- part8/library-frontend/src/components/SetBirthYear.jsx
+++ part8/library-frontend/src/components/SetBirthYear.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries/queries';
+import Select from 'react-select';
 
-const SetBirthYear = () => {
-  const [name, setName] = useState('');
+const SetBirthYear = ({ authors }) => {
+  const [name, setName] = useState(null);
   const [setBornTo, setSetBornTo] = useState('');
+
+  const options = authors.map((a) => {
+    return { value: a.name, label: a.name };
+  });
 
   const [editAuthor] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{ query: ALL_AUTHORS }],
@@ -18,20 +23,25 @@ const SetBirthYear = () => {
       variables: { name, setBornTo },
     });
     setName('');
-    setBornTo('');
+    setSetBornTo('');
+  };
+
+  const container = {
+    maxWidth: '400px',
   };
 
   return (
     <>
       <h2>Set BirthYear</h2>
       <form onSubmit={submit}>
-        <div>
-          name{' '}
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
+        <div style={container}>
+          <Select
+            defaultValue={name}
+            onChange={(target) => setName(target.value)}
+            options={options}
           />
         </div>
+
         <div>
           born
           <input
