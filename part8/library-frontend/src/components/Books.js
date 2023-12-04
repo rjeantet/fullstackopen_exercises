5b0f1b93eecd2a8cc7c4a2333b8b4c04c724e4ff
rjeantet <rose.jeantet@gmail.com>
2023-12-04 17:18:05 +0100

    8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,18 +1,35 @@
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries/queries';
+import { useEffect, useState } from 'react';
 
 const Books = () => {
   const result = useQuery(ALL_BOOKS);
   const books = result.data?.allBooks;
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    console.log('Books data:', books);
+    if (books && genres.length === 0) {
+      const extractedGenres = Array.from(
+        new Set(books.map((book) => book.genres).flat())
+      );
+      setGenres(extractedGenres);
+    }
+  }, [books]);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
+  const filteredBooks = selectedGenre
+    ? books.filter((book) => book.genres.includes(selectedGenre))
+    : books;
+  console.log('filteredBooks', filteredBooks);
+  console.log('genres', genres);
 
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -20,7 +37,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -29,6 +46,14 @@ const Books = () => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+        <button onClick={() => setSelectedGenre(null)}>all genres</button>
+      </div>
     </div>
   );
 };
--- part8/library-frontend/src/queries/queries.js
+++ part8/library-frontend/src/queries/queries.js
@@ -31,6 +31,7 @@ export const ALL_BOOKS = gql`
         name
       }
       published
+      genres
     }
   }
 `;
